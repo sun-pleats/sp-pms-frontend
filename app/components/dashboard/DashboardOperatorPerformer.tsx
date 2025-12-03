9796c8675b68f17662d05c0708b2d29418bcdbed
Kevin Loquencio <kevin.loquencio@gmail.com>
2025-12-04 03:51:20 +0800

    chore: fix dashboard operator performer

--- app/components/dashboard/DashboardOperatorPerformer.tsx
+++ app/components/dashboard/DashboardOperatorPerformer.tsx
@@ -26,23 +26,30 @@ const DashboardOperatorPerformer = ({ value, loading }: DashboardStatisticsProps
             <Skeleton width="100%" height="30px"></Skeleton>
           </div>
         ) : (
-          value?.map((operator, index) => (
-            <li
-              key={`dashboard-operator-performer-${index}`}
-              className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4"
-            >
-              <div>
-                <span className="text-900 font-medium mr-2 mb-1 md:mb-0">{operator.operator_name}</span>
-                <div className="mt-1 text-600">{operator.section_name}</div>
+          <>
+            {value?.length == 0 && (
+              <div className="col-12 flex justify-content-center align-items-center">
+                <p className="text-mute">No records found.</p>
               </div>
-              <div className="mt-2 md:mt-0 flex align-items-center">
-                <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{ height: '8px' }}>
-                  <div className="bg-orange-500 h-full" style={{ width: '50%' }} />
+            )}
+            {value?.map((operator, index) => (
+              <li
+                key={`dashboard-operator-performer-${index}`}
+                className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4"
+              >
+                <div>
+                  <span className="text-900 font-medium mr-2 mb-1 md:mb-0">{operator.operator_name}</span>
+                  <div className="mt-1 text-600">{operator.section_name}</div>
                 </div>
-                <span className="text-orange-500 ml-3 font-medium">{operator.avg_efficiency}%</span>
-              </div>
-            </li>
-          ))
+                <div className="mt-2 md:mt-0 flex align-items-center">
+                  <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{ height: '8px' }}>
+                    <div className="bg-orange-500 h-full" style={{ width: '50%' }} />
+                  </div>
+                  <span className="text-orange-500 ml-3 font-medium">{operator.avg_efficiency}%</span>
+                </div>
+              </li>
+            ))}
+          </>
         )}
       </ul>
     </div>
